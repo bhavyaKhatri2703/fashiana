4ecd3b61366031c24e4727b8b620a2ef17fa6040
bhavyakhatri2703 <khatribhavya2703@gmail.com>
2025-04-13 12:25:50 +0530

    twilio added

--- main.js
+++ main.js
@@ -10,14 +10,13 @@ const authToken = process.env.AUTH_TOKEN
 const client = twilio(accountSid, authToken);
 
 
-async function createMessage() {
+async function createMessage(text) {
     const message = await client.messages.create({
-      body: "Hello there!",
+      body: text,
       from: "whatsapp:+14155238886",
       to: "whatsapp:+918005987704",
     });
   
-    console.log(message.body);
   }
   
-  createMessage();
+  export default createMessage
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express"
 import bodyparser from "body-parser"
-
+import createMessage from "./main";
 
 
 const app = express();
@@ -14,7 +14,7 @@ app.post('/order', (req, res) => {
     const order = req.body
     const name = order.customer.first_name
     const total = order.total_price
-
+    createMessage(`${name} - ${total}`)
     console.log(`${name} - ${total}`)
     res.sendStatus(200)
   })
